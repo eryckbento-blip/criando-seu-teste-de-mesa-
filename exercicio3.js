6dbba8831b1056f48ea7a3da970b76cda8389b6d
eryckbento-blip <eryck.bento@escola.pr.gov.br>
2026-09-17 21:26:21 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,10 @@
+let valor = 150;
+let desconto = 0;
+
+if (valor > 100) {
+    desconto = valor * 0.10;
+}
+
+let valorFinal = valor - desconto;
+
+console.log(valorFinal);
---
+++ exercicio4.js
@@ -0,0 +1,10 @@
+let valor = 250;
+let desconto = 0;
+
+if (valor > 100) {
+    desconto = valor * 0.10;
+}
+
+let valorFinal = valor - desconto;
+
+console.log(valorFinal);
